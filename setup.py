fa08f83e6d065358c7aa2eab8d18b2831d9d8212
gr0vity-dev <85646666+gr0vity-dev@users.noreply.github.com>
2024-11-26 00:01:40 +0100

    websockets v14 breaks

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='nanows',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
-        'websockets'
+        'websockets>=10.3,<14'
     ],
     python_requires='>=3.6',
     description='A Python WebSocket API for interacting with Nano cryptocurrency nodes.',
